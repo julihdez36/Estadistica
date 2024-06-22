63d92140ed3fe7875a6a206ecaf97a8b2b0aace2
Hdez_J <jlopezhdez036@gmail.com>
2024-06-23 02:21:17 +1000

    0.2

--- Introduction.R
+++ Introduction.R
@@ -65,10 +65,15 @@ length(num_vector)
 length(char_vector)
 
 # Matriz
-## Arreglo bidimensional
+## Arreglo bidimensional donde los elementos son del mismo modo
 
-mat <- matrix(1:6, nrow = 2, ncol = 3)
+mat <- matrix(1:9,ncol = 3)
 print(mat)
+
+# Completa las matrices por columna, eso lo podemos cambiar
+
+mat_2 <- matrix(data = mat,byrow = T,nrow = 3)
+mat_2
 
 ## Una matriz puede ser la combinación de vectores
 
@@ -83,13 +88,28 @@ mat2 <- rbind(vec1, vec2)
 dim(mat1)
 dim(mat2)
 
+## Podemos darle nombre a las filas y columnas
+
+matrix(sample(x = 4:33,size = 8),ncol = 4,
+       dimnames = list(c('Individuo1','Individuo2'),
+                       c('Hermanos','Edad','Peso','Escolaridad')))
+
+#No es la forma habitual de trabajo con matrices
+
+
 # Arrays
 ## Generalización de matrices
 
 arr <- array(1:8, dim = c(2, 2, 2))
 print(arr)
 
+
 # Data frame
+
+# La limitación de las matrices y los arrays son los modos de los elementos
+# que contiene. Por tando, nosotros trabajaremos con marcos de datos 
+# o data frame
+
 
 df <- data.frame(
   id = 1:3,
@@ -99,6 +119,9 @@ df <- data.frame(
 print(df)
 
 # Listas
+
+## Es el objeto mas flexible, por lo que las salidas de los métodos 
+## Estadisticos que evaluaremos, suelen estár en formato lista
 
 lst <- list(
   num = 1:3,
@@ -113,5 +136,40 @@ print(lst)
 fact <- factor(c("low", "medium", "high", "medium", "low"))
 print(fact)
 
-# Pagina 24
 
+# Gráficos ----------------------------------------------------------------
+
+
+# Los gráficos en R se dividen 
+# Funciones gráficas de alto nivel: se crea un gráfico nuevo
+# Funciones gráficas de bajo nivel: modificamos uno ya existente
+
+# Para crearlo debemos construir los vectores
+
+x <- seq(-pi, pi,len = 100)
+plot(x, sin(x), type = 'l')
+
+# Mediante gráfico de dispersión
+
+plot(rnorm(50),rnorm(50)) # Gráfico de dispersión
+
+# Funciones de bajo nivel
+
+# Generar datos aleatorios de una distribución gamma
+set.seed(123)  # Para reproducibilidad
+datos_gamma <- rgamma(1000, shape = 2, scale = 2)
+
+# Generar el histograma
+hist(datos_gamma, main = "Histograma con Asimetría Positiva (Gamma)",
+     xlab = "Valores", breaks = 20, freq = F, col = 'lightblue')
+
+# Añadir líneas de densidad para una mejor visualización
+lines(density(datos_gamma), col = "red", lwd = 2)
+abline(v = mean(datos_gamma), col = 'purple', lwd = 2)
+abline(v = median(datos_gamma), col = 'green', lwd = 2)
+
+
+# Librerías ---------------------------------------------------------------
+
+install.packages("chemometrics")
+library(chemometrics)
